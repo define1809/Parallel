2f02eb201b301f5a5e5764f3f7a8eb98ba3e54e0
define1111 <s.vershkov.def@yandex.ru>
2022-10-17 22:37:55 +0300

    black version done

--- MonteCarloIntegration/ParallelMonteCarloIntegraion.c
+++ MonteCarloIntegration/ParallelMonteCarloIntegraion.c
@@ -1,9 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
-#include <time.h>
-
 #include <mpi.h>
 
 #define EXACT_SOLUTION (4.0*M_PI/3.0)
@@ -42,10 +39,7 @@ int main(int argc, char **argv)
     double I = 0.0;
     int start_idx = 0; 
     MPI_Status Status;
- //   int flag = 1;
-
-
-    srand(time(NULL));
+    int flag = 1;
 
     MPI_Init(&argc, &argv);
 
@@ -63,11 +57,13 @@ int main(int argc, char **argv)
 
     if (rank == 0)
     {
-        
-        //while (delta > eps)
-        //{
+        int d = 0;
+        double start = MPI_Wtime();
+        srand(start);
+//        MPI_Request request;        
+        while (1)
+        {
             points += PORTION_POINTS; 
-
             for (int rk = 1; rk < size; ++rk)
             {
                 for (size_t i = 0; i < portion_points; ++i)
@@ -75,22 +71,25 @@ int main(int argc, char **argv)
                     points_y[i] = rand01();
                     points_z[i] = rand01(); 
                 }
-
-//                flag = 1;
+                
                 MPI_Send(points_y, 
                          portion_points, 
                          MPI_DOUBLE, 
                          rk, 
                          0,
-                         MPI_COMM_WORLD); 
+                         MPI_COMM_WORLD);
+                    //     &request); 
                 MPI_Send(points_z, 
                          portion_points, 
                          MPI_DOUBLE, 
                          rk, 
                          0,
-                         MPI_COMM_WORLD);                
+                         MPI_COMM_WORLD);
+                     //    &request);                
             } 
-            
+           
+          //  MPI_Wait(&request, MPI_STATUS_IGNORE);
+ 
             MPI_Reduce(&integral_summ, 
                        &reduced_integral_summ, 
                        1, 
@@ -101,15 +100,45 @@ int main(int argc, char **argv)
 
             I = INTEGRATION_CONST * (reduced_integral_summ / points);
             delta = fabs(I - EXACT_SOLUTION);
-        //}  
-        
-        printf("I = %lf\ndelta = %lf\nn = %ld\n", I, delta, points);
+
+            if (delta < eps)
+            {
+                flag = 0;
+                for (int rk = 1; rk < size; ++rk)
+                    MPI_Send(&flag,
+                              1,
+                              MPI_INT,
+                              rk,
+                              0,
+                              MPI_COMM_WORLD);
+                          //    &request);    
+              //  MPI_Wait(&request, MPI_STATUS_IGNORE); 
+                break;
+            }   
+            else
+            {
+                for (int rk = 1; rk < size; ++rk)
+                    MPI_Send(&flag,
+                              1,
+                              MPI_INT,
+                              rk,
+                              0,
+                              MPI_COMM_WORLD);
+                           //   &request);
+               // MPI_Wait(&request, MPI_STATUS_IGNORE);
+            }
+        } 
+
+        double end = MPI_Wtime(); 
+       
+        printf("I = %lf\ndelta = %lf\npoints = %ld\ntime = %lf\n", I, delta,
+points, end - start);
 
     }
     else
     {
-       // while (flag)
-       // {
+        while (flag)
+        {
             MPI_Recv(points_y, 
                      portion_points, 
                      MPI_DOUBLE, 
@@ -126,9 +155,7 @@ int main(int argc, char **argv)
                      &Status);
 
             for (size_t i = 0; i < portion_points; ++i)
-            {
                 integral_summ += F(points_y[i], points_z[i]);
-            }     
             
             MPI_Reduce(&integral_summ,
                        &reduced_integral_summ,
@@ -137,7 +164,15 @@ int main(int argc, char **argv)
                        MPI_SUM,
                        0,
                        MPI_COMM_WORLD);
-        //} 
+
+            MPI_Recv(&flag,
+                     1,
+                     MPI_INT,
+                     0,
+                     0,
+                     MPI_COMM_WORLD,
+                     &Status);
+        } 
     }
 
 
